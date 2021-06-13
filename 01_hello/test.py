60cbe57b656c4606bd70542c00ec30f346a19b3b
Arseny.Tukhvatullin <arseny.tukhvatullin@muranosoft.com>
2021-06-13 14:06:39 +0300

    hello

--- 01_hello/test.py
+++ 01_hello/test.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!python3
 """tests for hello.py"""
 
 import os
 from subprocess import getstatusoutput, getoutput
 
-prg = './hello.py'
+prg = 'hello.py'
 
 
 # --------------------------------------------------
